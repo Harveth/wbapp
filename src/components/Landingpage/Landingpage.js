77ef4c5a453f23e99ec771dda281043c8b1224f9
Shehab <58978126+Solis3221@users.noreply.github.com>
2022-05-14 13:47:49 +0200

    Scrolling pages on landing page, some image assets

--- src/components/Landingpage/Landingpage.js
+++ src/components/Landingpage/Landingpage.js
@@ -8,7 +8,21 @@ class Landingpage extends Component {
       return (
         <div className='Landingpage'>
           <Navbar/>
-          
+          <div className='container'>
+            <div className='mlogo'>
+              <img src={require('../../Assets/Images/main_logo.png')} alt="lol"/>
+            </div>
+            <div className='menu'>
+              <p>
+            Check out the menu!
+            <button>Explore Menu</button>
+            </p>
+            <img src={require('../../Assets/Images/fish.png')} alt="lol"/>
+            </div>
+            <div>Two</div>
+            <div>Three</div>
+          </div>
+          <footer></footer>
         </div>
       );
     }
