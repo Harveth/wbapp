bdc8cdb91d34ca2f6d9e648775282876a697db5d
FeuPhoenix <amr.eldeeb172@gmail.com>
2022-06-07 23:04:11 +0200

    Styled Cart

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -21,25 +21,39 @@ class Cart extends Component {
         let currCart = this.getFoodFromCart();
       return (
 
-        <div>
-            <table>
+        <div className="smol-container cart-pg">
+            <table className="cart-table">
                 <thead>
                     <tr>
-                    <th>Item Name</th>
-                    <th>Quantity</th>
+                    <th className="cart-head">Item Name</th>
+                    <th className="cart-head">Quantity</th>
+                    <th className="cart-head">Subtotal</th>
                     </tr>
                 </thead>
                 <tbody>
                     {currCart.map(item => {
                     return (
                         <tr key={item.foodID}>
-                        <td>{ item.foodID }</td>
-                        <td>{ item.quantity}</td>
+                         <td className="cart-tbdata">
+                            <div className="cart-info">
+                                <img src="https://www.seriouseats.com/thmb/OBckE8o3ypWrULAwlkb11RvKD7w=/1000x1000/smart/filters:no_upscale()/20210714-potato-starch-fried-chicken-vicky-wasik-seriouseats-20-17e193a6bf274bba9091810a0b18ef89.jpg" width={200}></img>
+                                <div>
+                                <h5>{ item.foodID }</h5>
+                                <small>Price : 37$</small>
+                                <br />
+                                <a href="#">Remove</a>
+                                </div>
+                            </div>
+                         </td>
+                        <td className="cart-tbdata">{ item.quantity}</td>
+                        <td className="cart-tbdata"> 37$</td>
                         </tr>
+
                     );
                     })}
                 </tbody>
             </table>
+
         </div>
         );
     }
