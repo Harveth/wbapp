15e0cd3db1987c88dd4010a3d7f3f02fd9d597d2
Shehab <shehab.m.sadek@gmail.com>
2022-06-11 23:43:56 +0200

    Minor improvments, 404 fix and sing-up components

--- src/components/Not_found/Not_Found.js
+++ src/components/Not_found/Not_Found.js
@@ -1,12 +1,10 @@
 import { Component } from "react";
-import { Link } from 'react-router-dom';
-
 import './Not_Found.css';
 
 class Not_Found extends Component {
   render() {
     return (
-      <div className='row alignitems-center main-container404'>
+      <div className='row align-items-center main-container404'>
         <h1>404 - Not Found!</h1>
         <h4>The page you reqeusted does not exists or has been moved permenantly</h4>
       </div>
--- src/components/Signup/Signup.js
+++ src/components/Signup/Signup.js
@@ -1,25 +1,33 @@
 import { Component } from "react";
 import "./Signup.css";
-import { Navigate } from 'react-router-dom'
+import { Navigate } from "react-router-dom";
 import axios from "axios";
 const SERVER = "http://localhost:6969";
-
 
 class Signup extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      signUpFn:"",
-      signUpLn:"",
+      signUpFn: "",
+      signUpLn: "",
       signUpUsername: "",
       signUpPassword: "",
+      signUpPasswordC: "",
       signUpEmail: "",
       signUpPhoneNumber: "",
-      signUpSuccessful: false
+      signUpSuccessful: false,
     };
   }
+  // su(){
+  //   valid=false;
 
+  //   if(valid){
 
+  //   }
+  //   else{
+  //     sendServer();
+  //   }
+  // }
   su = async () => {
     await axios
       .post(`${SERVER}/ServerPHP/register.php`, {
@@ -28,7 +36,7 @@ class Signup extends Component {
         username: this.state.signUpUsername,
         phoneNumber: this.state.signUpPhoneNumber,
         firstname: this.state.signUpFn,
-        firstname: this.state.signUpln
+        firstname: this.state.signUpln,
       })
       .then((res) => {
         if (res.data == "error") {
@@ -37,7 +45,7 @@ class Signup extends Component {
           console.log(res.data);
           console.log(res.data.username);
           this.props.sendData(res.data.username);
-          this.setState({signUpSuccessful : true});
+          this.setState({ signUpSuccessful: true });
         }
       });
   };
@@ -57,88 +65,106 @@ class Signup extends Component {
   onPasswordChange = (event) => {
     this.setState({ signUpPassword: event.target.value });
   };
+  onPasswordCChange = (event) => {
+    this.setState({ signUpPasswordC: event.target.value });
+  };
   onPhoneNumberChange = (event) => {
     this.setState({ signUpPhoneNumber: event.target.value });
   };
-
 
   logState = () => {
     console.log(this.state);
   };
 
   render() {
-      if(this.state.signUpSuccessful){
-        <div className="alert alert-success" role="alert">
+    if (this.state.signUpSuccessful) {
+      <div className="alert alert-success" role="alert">
         <h4 className="alert-heading">Success!</h4>
         <p>You completed Sign up .</p>
-        <hr/>
-        <p className="mb-0">A request has been sent to the Quality Control department.</p>
-      </div>
-      }
+        <hr />
+        <p className="mb-0">
+          A request has been sent to the Quality Control department.
+        </p>
+      </div>;
+    }
     return (
       <body className="bgrnd1">
         <div className="Signup-Inputs">
-        <div className="containerMini">
-          <h1>Sign Up</h1>
-          <input
-            className="input-box"
-            placeholder="First name"
-            onChange={this.onFnChange}
-          ></input>
-          <br />
-          <input
-            className="input-box"
-            placeholder="Last name"
-            onChange={this.onLnChange}
-          ></input>
-          <br />
-          <input
-            className="input-box"
-            placeholder="Username"
-            onChange={this.onUsernameChange}
-          ></input>
-          <br />
-          <input
-            type="password"
-            className="input-box"
-            placeholder="Password"
-            onChange={this.onPasswordChange}
-          ></input>
-          <br />
-          <input
-            className="input-box"
-            placeholder="Email Address"
-            onChange={this.onEmailChange}
-          ></input>
-          <br />
-          <input
-            className="input-box"
-            placeholder="Phone number"
-            onChange={this.onPhoneNumberChange}
-          ></input>
+          <div className="containerMini">
+            <h1>Sign Up</h1>
+            <input
+              className="input-box"
+              placeholder="First name"
+              onChange={this.onFnChange}
+            ></input>
+            <br />
+            <input
+              className="input-box"
+              placeholder="Last name"
+              onChange={this.onLnChange}
+            ></input>
+            <br />
+            <input
+              className="input-box"
+              placeholder="Username"
+              onChange={this.onUsernameChange}
+            ></input>
+            <input
+              className="input-box"
+              placeholder="Email Address"
+              onChange={this.onEmailChange}
+            ></input>
+            <br />
+            <input
+              type="password"
+              className="input-box"
+              placeholder="Password"
+              onChange={this.onPasswordChange}
+            ></input>
+            <br />
+            <input
+              type="password"
+              className="input-box"
+              placeholder="Enter password again"
+              onChange={this.onPasswordCChange}
+            ></input>
+            <br />
+            <input
+              className="input-box"
+              placeholder="Phone number"
+              onChange={this.onPhoneNumberChange}
+            ></input>
 
-          <input type="file" id="actual-btn" hidden/>
-          <label for="actual-btn" className="lbl-btn">Choose File</label>
-          <span id="file-chosen" className="span-txt">Upload your national ID</span>
+            <input type="file" id="actual-btn" hidden />
+            <label for="actual-btn" className="lbl-btn">
+              Choose File
+            </label>
+            <span id="file-chosen" className="span-txt">
+              Upload your national ID
+            </span>
 
-          <input type="file" id="actual-btn" hidden/>
-          <label for="actual-btn" className="lbl-btn">Choose File</label>
-          <span id="file-chosen" className="span-txt">Upload a picture</span>
+            <input type="file" id="actual-btn" hidden />
+            <label for="actual-btn" className="lbl-btn">
+              Choose File
+            </label>
+            <span id="file-chosen" className="span-txt">
+              Upload a picture
+            </span>
 
-          <p className="smol">
-            Already have an account? <a href="/Signin">Sign in</a>
-          </p>
-          <p>
-            {" "}
-            <input type="checkbox"></input>I Agree to the{" "}
-            <a href="#">terms and conditions</a>{" "}
-          </p>
-          <button className="signup-btn" onClick={this.su}>
-            {" "}
-            Sign Up
-          </button>
+            <p className="smol">
+              Already have an account? <a href="/Signin">Sign in</a>
+            </p>
+            <p>
+              {" "}
+              <input type="checkbox"></input>I Agree to the{" "}
+              <a href="#">terms and conditions</a>{" "}
+            </p>
+            <button className="signup-btn" onClick={this.su}>
+              {" "}
+              Sign Up
+            </button>
+          </div>
         </div>
-      </div>
       </body>
     );
   }
